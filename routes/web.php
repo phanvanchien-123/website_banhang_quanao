2c481d9d89a1e0ebc35107588a87d0c8c1426248
Phan Chien <phanvanchien26032003@gmail.com>
2024-05-14 16:59:04 +0700

    đăng kí đăng nhập và migrate

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Client\AccountController;
 use App\Http\Controllers\materController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +16,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[materController::class,'index']);
-
+Route::middleware('auth')->group(function(){
+    Route::prefix('my_account')->group(function(){
+        Route::prefix('dashboard')->group(function(){
+            Route::get('/',[AccountController::class,'index'])->name('dashboard.index');
+        });
+    });
+});
 Auth::routes();
 
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
