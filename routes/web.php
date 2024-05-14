435a6d624603166d8acd4bd6b1cd9eeba59a729b
savallll <dhung993@gmail.com>
2024-05-14 09:04:01 +0700

    x

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/admin', function () {
     return view('admin.dashboard.index');
 });
 Route::get('/',[materController::class,'index']);
 
-Auth::routes();
+// Auth::routes();
 
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
