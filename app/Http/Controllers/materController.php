840a329f0779ace8455bd9a962685da355f57ae4
savallll <dhung993@gmail.com>
2024-05-14 09:38:23 +0700

    x

--- app/Http/Controllers/materController.php
+++ app/Http/Controllers/materController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class materController extends Controller
 {
     public function index(){
-        return view ('index1');
+        return view ('index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/admin', function () {
-    return view('admin.dashboard.index');
+    return view('welcome');
 });
 Route::get('/',[materController::class,'index']);
 
