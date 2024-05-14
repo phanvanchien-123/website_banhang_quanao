b82aadd00fe6dae04cbcbab73712b4a8b3abbc47
savallll <dhung993@gmail.com>
2024-05-14 17:45:08 +0700

    x

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -7,19 +7,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Skydash Admin</title>
     <!-- plugins:css -->
-    <link rel="stylesheet" href="{{ asset('admin/theme/vendors/feather/feather.css') }} ">
-    <link rel="stylesheet" href=" {{ asset('admin/theme/vendors/ti-icons/css/themify-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('admin/theme/vendors/css/vendor.bundle.base.css') }} ">
+    <link rel="stylesheet" href="{{ asset('theme_admin/theme/vendors/feather/feather.css') }} ">
+    <link rel="stylesheet" href=" {{ asset('theme_admin/theme/vendors/ti-icons/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('theme_admin/theme/vendors/css/vendor.bundle.base.css') }} ">
     <!-- endinject -->
     <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="{{ asset('admin/theme/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
-    <link rel="stylesheet" href="{{ asset('admin/theme/vendors/ti-icons/css/themify-icons.css') }} ">
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin/theme/js/select.dataTables.min.css') }} ">
+    <link rel="stylesheet" href="{{ asset('theme_admin/theme/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
+    <link rel="stylesheet" href="{{ asset('theme_admin/theme/vendors/ti-icons/css/themify-icons.css') }} ">
+    <link rel="stylesheet" type="text/css" href="{{ asset('theme_admin/theme/js/select.dataTables.min.css') }} ">
     <!-- End plugin css for this page -->
     <!-- inject:css -->
-    <link rel="stylesheet" href="{{ asset('admin/theme/css/vertical-layout-light/style.css') }} ">
+    <link rel="stylesheet" href="{{ asset('theme_admin/theme/css/vertical-layout-light/style.css') }} ">
     <!-- endinject -->
-    <link rel="shortcut icon" href="{{ asset('admin/theme/images/favicon.png') }} " />
+    <link rel="shortcut icon" href="{{ asset('theme_admin/theme/images/favicon.png') }} " />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
         integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -40,23 +40,23 @@
 
 
     <!-- plugins:js -->
-    <script src="{{ asset('admin/theme/vendors/js/vendor.bundle.base.js') }}"></script>
+    <script src="{{ asset('theme_admin/theme/vendors/js/vendor.bundle.base.js') }}"></script>
     <!-- endinject -->
     <!-- Plugin js for this page -->
-    <script src="{{ asset('admin/theme/vendors/chart.js/Chart.min.js') }}"></script>
-    <script src="{{ asset('admin/theme/vendors/datatables.net/jquery.dataTables.js') }} "></script>
-    <script src="{{ asset('admin/theme/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }} "></script>
-    <script src="{{ asset('admin/theme/js/dataTables.select.min.js') }} "></script>
+    <script src="{{ asset('theme_admin/theme/vendors/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('theme_admin/theme/vendors/datatables.net/jquery.dataTables.js') }} "></script>
+    <script src="{{ asset('theme_admin/theme/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }} "></script>
+    <script src="{{ asset('theme_admin/theme/js/dataTables.select.min.js') }} "></script>
 
     <!-- End plugin js for this page -->
     <!-- inject:js -->
-    <script src="{{ asset('admin/theme/js/off-canvas.js') }}"></script>
-    <script src="{{ asset('admin/theme/js/off-canvas.js') }}"></script>
-    <script src="{{ asset('admin/theme/js/off-canvas.js') }}"></script>
-    <script src="{{ asset('admin/theme/js/off-canvas.js') }}"></script>
-    <script src="{{ asset('admin/theme/js/off-canvas.js') }}"></script>
-    <script src="{{ asset('admin/theme/js/off-canvas.js') }}"></script>
-    <script src="{{ asset('admin/theme/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('theme_admin/theme/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('theme_admin/theme/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('theme_admin/theme/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('theme_admin/theme/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('theme_admin/theme/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('theme_admin/theme/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('theme_admin/theme/js/off-canvas.js') }}"></script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,69 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/admin', function () {
-    return view('welcome');
+Route::group(['namespace' => 'Backend', 'prefix' => 'admin'], function () {
+    // Route::get('',[Backend\HomeController::class,'index']) ->name('admin.home.index');
+    Route::get('', function () {
+        return view('admin.dashboard.index');
+    });
+    // Route::get('',[Backend\HomeController::class,'index']) ->name('admin.home.index');
+
+
+    Route::group(['prefix' => 'category' ] , function () {
+        Route::get('',[Backend\CategoryController::class,'index']) ->name('admin.category.index');
+        Route::get('create',[Backend\CategoryController::class,'create']) ->name('admin.category.create');
+        Route::post('store',[Backend\CategoryController::class,'store']) ->name('admin.category.store');
+
+        Route::get('edit/{id}',[Backend\CategoryController::class,'edit']) ->name('admin.category.edit');
+        Route::post('update/{id}',[Backend\CategoryController::class,'update']) ->name('admin.category.update');
+
+        Route::get('delete/{id}',[Backend\CategoryController::class,'delete']) ->name('admin.category.delete');      
+    });
+
+    Route::group(['prefix' => 'product' ] , function () {
+        Route::get('',[Backend\ProductController::class,'index']) ->name('admin.product.index');
+
+        Route::get('create',[Backend\ProductController::class,'create']) ->name('admin.product.create');
+        Route::post('store',[Backend\ProductController::class,'store']) ->name('admin.product.store');
+
+        Route::get('edit/{id}',[Backend\ProductController::class,'edit']) ->name('admin.product.edit');
+        Route::post('update/{id}',[Backend\ProductController::class,'update']) ->name('admin.product.update');
+
+        Route::get('delete/{id}',[Backend\ProductController::class,'delete']) ->name('admin.product.delete');      
+    });
+
+    Route::group(['prefix' => 'user' ] , function () {
+        Route::get('',[Backend\UserController::class,'index']) ->name('admin.user.index');
+
+        
+        Route::get('create',[Backend\UserController::class,'create']) ->name('admin.user.create');
+        Route::post('store',[Backend\UserController::class,'store']) ->name('admin.user.store');
+
+        Route::get('edit/{id}',[Backend\UserController::class,'edit']) ->name('admin.user.edit');
+        Route::post('update/{id}',[Backend\UserController::class,'update']) ->name('admin.user.update');
+
+        Route::get('delete/{id}',[Backend\UserController::class,'delete']) ->name('admin.user.delete');      
+    });
+
+    Route::group(['prefix' => 'profile' ] , function () {
+        Route::get('/{id}',[Backend\ProfileController::class,'show']) ->name('admin.profile.show');
+        Route::get('/updatePass/{id}',[Backend\ProfileController::class,'updatePass']) ->name('admin.profile.updatePass');
+        Route::post('/updatePass/{id}',[Backend\ProfileController::class,'update']);
+    });
+
+    Route::group(['prefix' => 'role','middleware' => ['role:System'] ] , function () {
+        Route::get('',[Backend\RoleController::class,'index']) ->name('admin.role.index');
+
+        
+        Route::get('create',[Backend\RoleController::class,'create']) ->name('admin.role.create');
+        Route::post('store',[Backend\RoleController::class,'store']) ->name('admin.role.store');
+
+        Route::get('edit/{id}',[Backend\RoleController::class,'edit']) ->name('admin.role.edit');
+        Route::post('update/{id}',[Backend\RoleController::class,'update']) ->name('admin.role.update');
+
+        Route::get('delete/{id}',[Backend\RoleController::class,'delete']) ->name('admin.role.delete');      
+    });
 });
-Route::get('/',[materController::class,'index']);
-
-// Auth::routes();
-
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 
